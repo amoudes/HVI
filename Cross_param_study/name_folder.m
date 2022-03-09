0cccc4c6e8b4ea23b4958f0d2c8f05493456672d
amoudes <60618095+amoudes@users.noreply.github.com>
2022-03-09 14:36:38 +0100

    update

---
+++ Cross_param_study/name_folder.m
@@ -0,0 +1,26 @@
+clc; close all; clear all;
+%%
+material = {'A2','S2','T4','M4'};
+t = [0.4, 0.8, 1.2];
+
+d = 9.58;
+td = t./d;
+
+v = [5.5, 6.7, 7.5];
+%%
+count = 0;
+for i = 1:length(material)
+    for j = 1:length(td)
+        for k = 1:length(v)
+            count = count + 1;
+            name = [num2str(count),'_',material{i},'_td',num2str(td(j),3),'_v',num2str(v(k))];
+            fprintf('%s\n',name);
+
+            if ~exist(name, 'dir')
+                mkdir(name)
+            end
+
+        end
+    end
+end
+
